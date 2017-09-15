783b9a77e791fc4a4f48256ed847a719e8b7c962
Galder Zamarreño <galder@zamarreno.com>
2017-09-15 10:04:13 +0200

    Mimic trains moving with different position snapshots

--- README.md
+++ README.md
@@ -18,5 +18,7 @@ You should see these two lines:
     Listening at localhost: 3000
     webpack built ... in NNNNmss
 
-The UI currently runs taking a snapshot of a REST endpoint and displaying that.
+The UI currently runs taking a snapshot of a REST endpoint with all the current train positions and displaying that.
+The UI periodically requests this snapshot, default is 3 seconds.
 We could consider more optimal ways of doing it? Or more streaming based? HTTP/2?
+Might be a nice improvement after first workshop, need Thomas' input on this.
--- delayed-train-positions/src/main/java/delayed/train/positions/HttpApp.java
+++ delayed-train-positions/src/main/java/delayed/train/positions/HttpApp.java
@@ -10,6 +10,8 @@ import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class HttpApp extends AbstractVerticle {
 
+   private int i = 0;
+
    // Convenience method so you can run it in your IDE
    public static void main(String[] args) {
       Runner r = new Runner("delayed-train-positions/src/main/java/");
@@ -39,19 +41,43 @@ public class HttpApp extends AbstractVerticle {
    }
 
    private void position(RoutingContext rc) {
+      int pos = this.i++ % 3;
+      System.out.println("Retrieve /position [" + pos +"]");
       rc.response()
          .putHeader("Access-Control-Allow-Origin", "*")
-         //.putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
          .end(
-            "train_id\ttrain_category\ttrain_name\ttrain_lastStopName\tposition_lat\tposition_lng\tposition_bearing\n" +
-               "84/80849/18/30/95\tS\tS 3\tSt. Margrethen\t47.474748\t9.481462\t0.0\n" +
-               "84/414330/18/21/95\tRE\tRE 2575\tMilano Centrale\t46.175546\t10.146762\t190.0"
-//               "84/364141/18/19/95\tTER\tTER96456\tPontarlier (F)\t46.854834\t6.176659\t110.0\n" +
-//               "84/451623/18/19/95\tGEX\tGEX 903\tZermatt\t46.03891\t7.761331\t210.0\n" +
-//               "84/242400/18/19/95\tRE\tRE 1354\tLandquart\t46.907376\t9.810728\t130.0\n" +
-//               "84/269912/18/19/95\tIRE\tIRE 4211\tLindau Hbf\t47.742509\t9.623716\t210.0\n" +
-//               "84/25571/18/21/95\tICN\tICN 526\tGenève-Aéroport\t46.886584\t6.767367\t200.0\n"
+            positions()[pos]
          );
+   }
+
+   private String[] positions() {
+      return new String[]{
+         "train_id\ttrain_category\ttrain_name\ttrain_lastStopName\tposition_lat\tposition_lng\tposition_bearing\n" +
+            "84/80849/18/30/95\tS\tS 3\tSt. Margrethen\t47.474748\t9.481462\t0.0\n" +
+            "84/414330/18/21/95\tRE\tRE 2575\tMilano Centrale\t46.175546\t10.146762\t190.0\n" +
+            "84/364141/18/19/95\tTER\tTER96456\tPontarlier (F)\t46.854834\t6.176659\t110.0\n" +
+            "84/451623/18/19/95\tGEX\tGEX 903\tZermatt\t46.03891\t7.761331\t210.0\n" +
+            "84/242400/18/19/95\tRE\tRE 1354\tLandquart\t46.907376\t9.810728\t130.0\n" +
+            "84/25571/18/21/95\tICN\tICN 526\tGenève-Aéroport\t46.886584\t6.767367\t200.0"
+         ,
+         "train_id\ttrain_category\ttrain_name\ttrain_lastStopName\tposition_lat\tposition_lng\tposition_bearing\n" +
+            "84/80849/18/30/95\tS\tS 3\tSt. Margrethen\t47.475152\t9.487655\t0.0\n" +
+            "84/414330/18/21/95\tRE\tRE 2575\tMilano Centrale\t46.172813\t10.142475\t190.0\n" +
+            "84/364141/18/19/95\tTER\tTER96456\tPontarlier (F)\t46.865099\t6.163211\t110.0\n" +
+            "84/451623/18/19/95\tGEX\tGEX 903\tZermatt\t46.037202\t7.759758\t210.0\n" +
+            "84/242400/18/19/95\tRE\tRE 1354\tLandquart\t46.90823\t9.806602\t140.0\n" +
+            "84/269912/18/19/95\tIRE\tIRE 4211\tLindau Hbf\t47.737449\t9.620462\t210.0\n" +
+            "84/25571/18/21/95\tICN\tICN 526\tGenève-Aéroport\t46.879141\t6.759222\t200.0"
+         ,
+         "train_id\ttrain_category\ttrain_name\ttrain_lastStopName\tposition_lat\tposition_lng\tposition_bearing\n" +
+            "84/80849/18/30/95\tS\tS 3\tSt. Margrethen\t47.475925\t9.493354\t10.0\n" +
+            "84/414330/18/21/95\tRE\tRE 2575\tMilano Centrale\t46.167339\t10.133917\t190.0\n" +
+            "84/364141/18/19/95\tTER\tTER96456\tPontarlier (F)\t46.87096\t6.155525\t110.0\n" +
+            "84/451623/18/19/95\tGEX\tGEX 903\tZermatt\t46.035269\t7.75796\t210.0\n" +
+            "84/242400/18/19/95\tRE\tRE 1354\tLandquart\t46.90965\t9.798466\t140.0\n" +
+            "84/269912/18/19/95\tIRE\tIRE 4211\tLindau Hbf\t47.729403\t9.609414\t200.0\n" +
+            "84/25571/18/21/95\tICN\tICN 526\tGenève-Aéroport\t46.865099\t6.741451\t200.0"
+      };
    }
 
 }
