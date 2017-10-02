00ec1c0559ba593bca9e9ee7664ba2c822394fe9
Galder Zamarreño <galder@zamarreno.com>
2017-10-02 07:18:36 +0200

    Reformat code

--- delayed-train-positions/src/main/java/delayed/train/positions/Runner.java
+++ delayed-train-positions/src/main/java/delayed/train/positions/Runner.java
@@ -13,72 +13,72 @@ public class Runner {
 //   private static final String DIR = "board-rest";
 //   private static final String JAVA_DIR = DIR + "/src/test/java/";
 
-   private final String javaDir;
+  private final String javaDir;
 
-   public Runner(String javaDir) {
-      this.javaDir = javaDir;
-   }
+  public Runner(String javaDir) {
+    this.javaDir = javaDir;
+  }
 
-   public void runClusteredExample(Class clazz) {
-      runExample(javaDir, clazz, new VertxOptions().setClustered(true), null);
-   }
+  public void runClusteredExample(Class clazz) {
+    runExample(javaDir, clazz, new VertxOptions().setClustered(true), null);
+  }
 
-   public void runExample(Class clazz) {
-      runExample(javaDir, clazz, new VertxOptions().setClustered(false), null);
-   }
+  public void runExample(Class clazz) {
+    runExample(javaDir, clazz, new VertxOptions().setClustered(false), null);
+  }
 
-   public void runExample(Class clazz, DeploymentOptions options) {
-      runExample(javaDir, clazz, new VertxOptions().setClustered(false), options);
-   }
+  public void runExample(Class clazz, DeploymentOptions options) {
+    runExample(javaDir, clazz, new VertxOptions().setClustered(false), options);
+  }
 
-   public static void runExample(String exampleDir, Class clazz, VertxOptions options, DeploymentOptions
-         deploymentOptions) {
-      runExample(exampleDir + clazz.getPackage().getName().replace(".", "/"), clazz.getName(), options, deploymentOptions);
-   }
+  public static void runExample(String exampleDir, Class clazz, VertxOptions options, DeploymentOptions
+    deploymentOptions) {
+    runExample(exampleDir + clazz.getPackage().getName().replace(".", "/"), clazz.getName(), options, deploymentOptions);
+  }
 
-   public static void runExample(String exampleDir, String verticleID, VertxOptions options, DeploymentOptions deploymentOptions) {
-      if (options == null) {
-         // Default parameter
-         options = new VertxOptions();
+  public static void runExample(String exampleDir, String verticleID, VertxOptions options, DeploymentOptions deploymentOptions) {
+    if (options == null) {
+      // Default parameter
+      options = new VertxOptions();
+    }
+    // Smart cwd detection
+
+    // Based on the current directory (.) and the desired directory (exampleDir), we try to compute the vertx.cwd
+    // directory:
+    try {
+      // We need to use the canonical file. Without the file name is .
+      File current = new File(".").getCanonicalFile();
+      if (exampleDir.startsWith(current.getName()) && !exampleDir.equals(current.getName())) {
+        exampleDir = exampleDir.substring(current.getName().length() + 1);
       }
-      // Smart cwd detection
+    } catch (IOException e) {
+      // Ignore it.
+    }
 
-      // Based on the current directory (.) and the desired directory (exampleDir), we try to compute the vertx.cwd
-      // directory:
+    System.setProperty("vertx.cwd", exampleDir);
+    Consumer<Vertx> runner = vertx -> {
       try {
-         // We need to use the canonical file. Without the file name is .
-         File current = new File(".").getCanonicalFile();
-         if (exampleDir.startsWith(current.getName()) && !exampleDir.equals(current.getName())) {
-            exampleDir = exampleDir.substring(current.getName().length() + 1);
-         }
-      } catch (IOException e) {
-         // Ignore it.
+        if (deploymentOptions != null) {
+          vertx.deployVerticle(verticleID, deploymentOptions);
+        } else {
+          vertx.deployVerticle(verticleID);
+        }
+      } catch (Throwable t) {
+        t.printStackTrace();
       }
-
-      System.setProperty("vertx.cwd", exampleDir);
-      Consumer<Vertx> runner = vertx -> {
-         try {
-            if (deploymentOptions != null) {
-               vertx.deployVerticle(verticleID, deploymentOptions);
-            } else {
-               vertx.deployVerticle(verticleID);
-            }
-         } catch (Throwable t) {
-            t.printStackTrace();
-         }
-      };
-      if (options.isClustered()) {
-         Vertx.clusteredVertx(options, res -> {
-            if (res.succeeded()) {
-               Vertx vertx = res.result();
-               runner.accept(vertx);
-            } else {
-               res.cause().printStackTrace();
-            }
-         });
-      } else {
-         Vertx vertx = Vertx.vertx(options);
-         runner.accept(vertx);
-      }
-   }
+    };
+    if (options.isClustered()) {
+      Vertx.clusteredVertx(options, res -> {
+        if (res.succeeded()) {
+          Vertx vertx = res.result();
+          runner.accept(vertx);
+        } else {
+          res.cause().printStackTrace();
+        }
+      });
+    } else {
+      Vertx vertx = Vertx.vertx(options);
+      runner.accept(vertx);
+    }
+  }
 }
