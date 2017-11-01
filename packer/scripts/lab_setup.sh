e6ee3f4d72b6f918dcb4bb6143211b3573364c05
Thomas Segismont <tsegismo@redhat.com>
2017-11-01 22:24:41 +0100

    Start Openshift and deploy ISPN + apps to make the VM usable offline

--- packer/scripts/lab_setup.sh
+++ packer/scripts/lab_setup.sh
@@ -3,32 +3,41 @@
 set -e
 set -x
 
-export M2_HOME=${HOME}/maven
-export PATH=${M2_HOME}/bin:${PATH}
+WORKSHOP=streaming-data-workshop
+WORKSHOP_DIR=${HOME}/${WORKSHOP}
 
-echo "#################################################################"
-echo "Checking Maven install"
-echo
-mvn -version
-echo
-echo "Done"
-echo "#################################################################"
+git clone https://github.com/infinispan-demos/${WORKSHOP} ${WORKSHOP_DIR}
 
-echo "#################################################################"
-echo "Checking NVM install"
-echo
-command -v nvm
-echo
-echo "Done"
-echo "#################################################################"
-
-cd ${HOME}
-git clone https://github.com/infinispan-demos/streaming-data-workshop
-cd streaming-data-workshop
-
+cd ${WORKSHOP_DIR}
 mvn install dependency:go-offline
 
-cd web-viewer
+cd ${WORKSHOP_DIR}/web-viewer
 nvm use 4.2
 npm install
 npm run build
+
+cd ${WORKSHOP_DIR}
+./start-openshift.sh
+./start-datagrid.sh
+
+(cd ./data-model; ./install.sh)
+(cd ./workshop-main; ./deploy.sh)
+(cd ./stations-injector; ./deploy.sh)
+(cd ./positions-injector; ./deploy.sh)
+(cd ./delayed-listener; ./deploy.sh)
+(cd ./delayed-trains; ./deploy.sh)
+(cd ./datagrid-visualizer; ./deploy.sh)
+
+# Expecting 6 app pods plus 3 ISPN pods
+EXPECTED_PODS=9
+START=$(date -u +%s)
+until [ "$(oc get pods -a=false -o=name | wc -l)" -eq "${EXPECTED_PODS}" ]; do
+    printf '.'
+    sleep 5
+    if [ "$(bc <<< "$(date -u +%s)-${START}")" -gt "15" ]; then
+        # Timeout
+        exit 128
+    fi
+done
+
+oc cluster down
