a4d7caa6135a6f5b97e3c06c62f2a0cf61712854
Galder Zamarreño <galder@zamarreno.com>
2017-11-20 13:50:45 +0100

    Update steps in delayed trains position exercise

--- deploy-solution.sh
+++ deploy-solution.sh
@@ -4,12 +4,10 @@ set -e -x
 
 mvn clean install -N
 (cd ./data-model; mvn install)
-(cd ./workshop-main; mvn fabric8:deploy -Psolution)
+(cd ./workshop-main; mvn fabric8:deploy)
 (cd ./stations-injector; mvn fabric8:deploy -Psolution)
 (cd ./positions-injector; mvn fabric8:deploy)
 (cd ./delayed-listener; mvn fabric8:deploy -Psolution)
 (cd ./delayed-trains; mvn fabric8:deploy -Psolution)
 (cd ./datagrid-visualizer; ./deploy.sh)
 (cd ./simple-web-application; mvn fabric8:deploy -Psolution)
-
-(cd ./web-viewer; ./start.sh)
