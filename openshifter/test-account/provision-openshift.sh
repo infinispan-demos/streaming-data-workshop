2c30856fdeb9a17e32c30f03f8056308c164327a
Galder Zamarreño <galder@zamarreno.com>
2018-04-18 18:15:58 +0200

    Add script to provision OpenShift from within Google and update steps

---
+++ openshifter/test-account/provision-openshift.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e -x
+
+git clone https://github.com/infinispan-demos/streaming-data-workshop
+cd streaming-data-workshop/openshifter/test-account
+
+gcloud compute project-info add-metadata \
+  --metadata google-compute-default-region=europe-west1,google-compute-default-zone=europe-west1-b
+
+export PROJECT_ID=$(gcloud config get-value core/project)
+
+gcloud iam service-accounts create openshifter \
+  --display-name "OpenShifter Service Account"
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member serviceAccount:openshifter@$PROJECT_ID.iam.gserviceaccount.com \
+  --role roles/editor
+gcloud iam service-accounts keys create openshifter.json \
+  --iam-account openshifter@$PROJECT_ID.iam.gserviceaccount.com
+
+ssh-keygen -f openshifter-key -q -N ""
+
+sed -i -e 's/REPLACE_ME/'"$PROJECT_ID"'/g' streaming-data-workshop.yml
+
+docker run -e -ti -v `pwd`:/root/data docker.io/osevg/openshifter create streaming-data-workshop
